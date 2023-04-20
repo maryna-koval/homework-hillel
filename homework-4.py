05de32984a235d84995c60c033a59e4e3d43ac2a
marina <maryna.koval1295@gmail.com>
2023-04-19 17:26:31 -0700

    Homework 2 updated

--- homework-4.py
+++ homework-4.py
@@ -1,6 +1,8 @@
 from math import sqrt
-a = float(input('Enter first side: '))
-b = float(input('Enter second side: '))
-c = float(input('Enter third side: '))
+a = int(input('Enter first side: '))
+b = int(input('Enter second side: '))
+c = int(input('Enter third side: '))
+
 p = (a + b + c)/2
+
 print(f'Triangle area is {sqrt(p*(p-a)*(p-b)*(p-c))}')
