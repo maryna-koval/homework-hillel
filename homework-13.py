0a50a71758f97e7cb6af708a91e7510b42e5ac20
marina <maryna.koval1295@gmail.com>
2023-05-21 11:47:05 -0700

    homework 13 fixed

--- homework-13.py
+++ homework-13.py
@@ -3,28 +3,21 @@ file_permissions = {}
 for i in range(n):
     user_input = input("Enter name of files and permissions (example, 'file.txt R W X'): ")
     input_parts = user_input.split()
-    file = input_parts[0]
-    permissions = input_parts[1:]
-    file_permissions[file] = permissions
+    file, *permissions = input_parts
+    file_permissions[file] = [perm.lower() for perm in permissions]
 m = int(input("Enter amount of requests: "))
 queries = []
 files = []
 for i in range(m):
     query, file = input("Enter your request (example, 'read file.txt'): ").split()
-    queries.append(query)
-    files.append(file)
+    queries.append(query.lower())
+    files.append(file.lower())
 for i in range(m):
     query = queries[i]
     file = files[i]
-    if file in file_permissions:
-        permissions = file_permissions[file]
-        if query == "read" and "R" in permissions:
-            print("OK")
-        elif query == "write" and "W" in permissions:
-            print("OK")
-        elif query == "execute" and "X" in permissions:
-            print("OK")
-        else:
-            print("Access denied")
+    permissions = file_permissions.get(file, [])
+    if query == "read" and "r" in permissions or query == 'write' and "w" in permissions \
+            or query == "execute" and "x" in permissions:
+        print("OK")
     else:
         print("Access denied")
