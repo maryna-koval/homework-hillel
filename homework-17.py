bfa99dd4722fe284b1afa30d5f305935711562b2
marina <maryna.koval1295@gmail.com>
2023-06-14 17:15:47 -0700

    homework 17 fixed

--- homework-17.py
+++ homework-17.py
@@ -3,29 +3,28 @@ def change(lst):
         lst[0], lst[-1] = lst[-1], lst[0]
     else:
         print("У списку повинно бути щонайменше 2 елементи.")
-    print(lst)
+    return lst
 
 
 new_list = [2, 4, 5, 6]
-change(new_list)
+print(change(new_list))
 
 
 def to_dict(lst):
-     dictionary = dict((x, x) for x in lst)
-     print(dictionary)
+    return {x: x for x in lst}
 
 
 new_list = ['a', 'b', 'c', 'd']
-to_dict(new_list)
+print(to_dict(new_list))
 
 
 def sum_range(start, end):
-     if start > end:
-          start, end = end, start
-     result = sum(range(start, end + 1))
-     print("Сума чисел:", result)
+    if start > end:
+        start, end = end, start
+    result = sum(range(start, end + 1))
+    return result
 
 
 start = int(input("Введіть значення start: "))
 end = int(input("Введіть значення end: "))
-sum_range(start, end)
+print(f'Сума чисел:{sum_range(start, end)}')
